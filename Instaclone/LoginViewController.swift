57e12b23d4495476026fc89d4e4881d0a0beb5f4
KarstenKrause <karsten.krause1988@gmail.com>
2021-01-02 22:32:39 +0100

    Login/Registrierung - UI Interaktionen fertig

--- Instaclone/LoginViewController.swift
+++ Instaclone/LoginViewController.swift
@@ -8,22 +8,77 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    // MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViewComponents()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    
+    func setupViewComponents() {
+        setupEmailTextField()
+        setupPasswordTextField()
+        addTargetToTextfields()
+        setupInitialLoginButton()
     }
-    */
+    
+    func setupEmailTextField() {
+        emailTextField.backgroundColor = UIColor(white: 1.0, alpha: 0.1)
+        emailTextField.borderStyle = .roundedRect
+    }
+    
+    func setupPasswordTextField() {
+        passwordTextField.backgroundColor = UIColor(white: 1.0, alpha: 0.1)
+        passwordTextField.borderStyle = .roundedRect
+    }
+    
+    func setupInitialLoginButton() {
+        loginButton.layer.cornerRadius = 5
+        loginButton.backgroundColor = UIColor(red: 0, green: 0.6, blue: 1.0, alpha: 0.4)
+        loginButton.setTitleColor(UIColor(white: 1.0, alpha: 0.4), for: .normal)
+        loginButton.isEnabled = false
+    }
+    
+    func addTargetToTextfields() {
+        emailTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+    }
+    
+    /// Checks that both textfields have an input. If so, the login button is enabled
+    @objc func textFieldChanged() {
+        let textInTextFields: Bool = emailTextField.text?.count ?? 0 > 0 && passwordTextField.text?.count ?? 0 > 0
+        
+        if textInTextFields {
+            loginButton.layer.cornerRadius = 5
+            loginButton.backgroundColor = UIColor(red: 0, green: 0.6, blue: 1.0, alpha: 1.0)
+            loginButton.setTitleColor(UIColor(white: 1.0, alpha: 1.0), for: .normal)
+            loginButton.isEnabled = true
+        } else {
+            setupInitialLoginButton()
+        }
+    }
+    
+    // MARK: Dismiss Keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        view.endEditing(true)
+        print("login button tapped")
+    }
+    
+    
 
 }
--- Instaclone/RegistrationViewController.swift
+++ Instaclone/RegistrationViewController.swift
@@ -9,22 +9,78 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registrationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViewComponents()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    func setupViewComponents() {
+        setupUsernameTextField()
+        setupEmailTextField()
+        setupPasswordTextField()
+        addTargetToTextFields()
+        setupInitialRegistrationButton()
     }
-    */
+
+    func setupUsernameTextField() {
+        usernameTextField.backgroundColor = UIColor(white: 1.0, alpha: 0.1)
+        usernameTextField.borderStyle = .roundedRect
+    }
+    
+    func setupEmailTextField() {
+        emailTextField.backgroundColor = UIColor(white: 1.0, alpha: 0.1)
+        emailTextField.borderStyle = .roundedRect
+    }
+    
+    func setupPasswordTextField() {
+        passwordTextField.backgroundColor = UIColor(white: 1.0, alpha: 0.1)
+        passwordTextField.borderStyle = .roundedRect
+    }
+    
+    func setupInitialRegistrationButton() {
+        registrationButton.layer.cornerRadius = 5
+        registrationButton.backgroundColor = UIColor(red: 0, green: 0.6, blue: 1.0, alpha: 0.4)
+        registrationButton.setTitleColor(UIColor(white: 1.0, alpha: 0.4), for: .normal)
+        registrationButton.isEnabled = false
+    }
+    
+    func addTargetToTextFields() {
+        usernameTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        emailTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+    }
+    
+    /// Checks that all three textfields have an input. If so, the registration button is enabled
+    @objc func textFieldChanged() {
+        let textInTextFields: Bool = usernameTextField.text?.count ?? 0 > 0 && emailTextField.text?.count ?? 0 > 0 && passwordTextField.text?.count ?? 0 > 0
+        
+        if textInTextFields {
+            registrationButton.layer.cornerRadius = 5
+            registrationButton.backgroundColor = UIColor(red: 0, green: 0.6, blue: 1.0, alpha: 1)
+            registrationButton.setTitleColor(UIColor(white: 1.0, alpha: 1), for: .normal)
+            registrationButton.isEnabled = true
+        } else {
+            setupInitialRegistrationButton()
+        }
+    }
+    
+    // MARK: Dismiss Keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    // MARK: - Actions
+    @IBAction func registrationButtonTapped(_ sender: UIButton) {
+        view.endEditing(true)
+        print("registration button tapped")
+    }
     
     
     @IBAction func dismissButtonTapped(_ sender: UIButton) {
