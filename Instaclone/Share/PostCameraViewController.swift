cbc543760cbd515ed3d18754948455dba64ea8c5
KarstenKrause <karsten.krause1988@gmail.com>
2021-01-26 13:18:07 +0100

    Kamera mit Hilfe von AVFoundation aufgesetzt

--- Instaclone/Share/PostCameraViewController.swift
+++ Instaclone/Share/PostCameraViewController.swift
@@ -6,18 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
+import ProgressHUD
 
 class PostCameraViewController: UIViewController {
+    // MARK: - Outlets
     @IBOutlet weak var previewPhotoView: UIImageView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var cameraSwitchButton: UIButton!
     
+    // MARK: - Properties
+    var captureSession = AVCaptureSession()
+    var photoOutput = AVCapturePhotoOutput()
     
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupInitalButtonDedigns()
+        setupCaptureSession()
         
     }
     
@@ -28,6 +37,45 @@ class PostCameraViewController: UIViewController {
         cancelButton.tintColor = .white
         cameraButton.tintColor = .white
         cameraSwitchButton.tintColor = .white
+    }
+    
+    
+    /// Setting up the session for the camera.
+    func setupCaptureSession() {
+        // 1. Capture Session - quality of the captured photo
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        // 2. Input
+        let captureDevice = AVCaptureDevice.default(for: .video)
+        do {
+            guard let device = captureDevice else { return }
+            let input = try AVCaptureDeviceInput(device: device)
+            
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+        } catch let error {
+            ProgressHUD.showError(error.localizedDescription)
+        }
+        
+        // 3. Output
+        photoOutput.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey : AVVideoCodecType.jpeg])], completionHandler: nil)
+        
+        if captureSession.canAddOutput(photoOutput) {
+            captureSession.addOutput(photoOutput)
+        }
+        
+        // 4. Previewlayer - presents the taken photo
+        let cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer.videoGravity = .resizeAspectFill
+        cameraPreviewLayer.connection?.videoOrientation = .portrait
+        cameraPreviewLayer.frame = view.frame
+        
+        view.layer.insertSublayer(cameraPreviewLayer, at: 0)
+        
+        // 5. Start
+        captureSession.startRunning()
+        
     }
     
     
